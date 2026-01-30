99ad15e39a77cda9df20623a97d10875465c38b9
Pratham2005-analyze <tagrapratham1979@gmail.com>
2026-01-30 23:50:20 +0530

    Add files via upload

---
+++ mathematcal_operations.py
@@ -0,0 +1,13 @@
+#Taking inputs
+num1 = int(input("enter the first number:"))
+num2 = int(input("enter the second number:"))
+# Performing Operations
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+#results
+print("addition:",addition)
+print("subtraction:",subtraction)
+print("multiplication:",multiplication)
+print("division:",division)
